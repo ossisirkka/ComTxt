7c1f500d0eaf21bec97cfda2b9c7edaf3865dfa9
Local Administrator <LocalAdmin@TUD277510.tudelft.net>
2021-03-24 16:50:41 +0100

    message

--- R/topic_network.R
+++ R/topic_network.R
@@ -1,64 +1,63 @@
-tabPanel("Mallet words cloud",
-         sidebarLayout(
-           sidebarPanel(
+## semantic network for mallet
 
-             # Input: Checkbox if file has url
-             numericInput("mt_wc", "Num words in cloud", 200, min = 1, max = 800),
+topic_network <- function(mallet_df, degree_weight = 20, igraph_semantic_vertex = 0.4, igraph_semantic_label= 20){
+  topic.words <- mallet.topic.words(mallet_df, smoothed = T, normalized = T)
 
-             # Input: Checkbox if file has url
-             #numericInput("n_t", "Choose topic number", 1, min = 1, max = 100),
-             # Input: Checkbox if file has url
-             numericInput("par_row", "screen layout row",value = 3, min = 1, max = 10),
-             # Input: Checkbox if file has url
-             numericInput("par_col", "screen layout col",value = 4, min = 1, max = 10),
-             # Horizontal line
-             tags$hr(),
-
-             ##action button
-             actionButton("malletcloud", "Mallet topic wordcloud", class = "btn-primary")
-           ),
-
-
-           # Main panel for displaying outputs
-           mainPanel(
-
-             # Output: Data file
-             plotOutput("mallet_wordscloud",width = "100%", height = "600px")
-
-           )
-         )
-)
-
-
-plot_mallet <- eventReactive(input$malletcloud,{
-  withProgress(message = 'Progress for plot..', value = 10, {
-    req(topic_mallet_list())
-    pal2 <- brewer.pal(8,"Dark2")
-    #df_words <- as.data.frame(topic_mallet_list()[input$n_t])
-    #wordcloud(df_words$words, df_words$weights, scale = c(5,.5), #min.freq = 100,
-    #max.words = input$mt_wc, random.order = FALSE, rot.per = .15, colors = pal2, main = paste0("wordcloud topic", input$n_t))
-    par(mfrow = c(input$par_row, input$par_col),     # 2x2 layout
-        oma = c(2, 2, 0, 0), # two rows of text at the outer left and bottom margin
-        mar = c(1, 1, 0, 0), # space for one row of text at ticks and to separate plots
-        mgp = c(2, 1, 0),    # axis label at 2 rows distance, tick labels at 1 row
-        xpd = NA)
-
-    for(i in 1:length(topic_mallet_list())){
-      df_words <- as.data.frame(topic_mallet_list()[i])
-      wordcloud(df_words$words, df_words$weights, scale = c(3,.2), #min.freq = 100,
-                max.words = input$mt_wc, random.order = FALSE, rot.per = .15, colors = pal2, main = "Mallet wordcloud")
+  tmp.1 <- list()
+  for(j in 1:nrow(topic.words)){
+    topic.df <- mallet.top.words(mallet_df, topic.words[j,],nrow(t(topic.words)))
+    #topic.df <- topic.df[topic.df$words != "cultura",]
+    topic.df <- topic.df[topic.df$weight >= degree_weight,]
+    tmp <- list()
+    for(i in 1:nrow(topic.df )){
+      from <- topic.df[i,1]
+      to <- topic.df[topic.df$words != from,]$words
+      weight <- list()
+      for(z in 1:length(to)){
+        weight.1 <- topic.df[i,2]
+        weight.2 <- topic.df[topic.df$words == to[z],]$weights
+        weight.3 <- weight.1+weight.2
+        weight.3 <- weight.3/2
+        weight[[z]] <- weight.3/nrow(topic.df)
+      }
+      weight <- unlist(weight)
+      tmp[[i]] <- data.frame(from = from, to = to, weight = weight)
     }
-  })
-})
+    tmp <- do.call("rbind", tmp)
+    tmp$topic <- paste("topic",j)
+    tmp.1[[j]] <- tmp
+  }
 
-output$mallet_wordscloud <- renderPlot({
-  #req(topic_mallet_list())
-  #pal2 <- brewer.pal(8,"Dark2")
-  #par(mfrow=c(3,4))
-  #for(i in 1:10){
-  #   df_words <- as.data.frame(topic_mallet_list()[i])
-  #  wordcloud(df_words$words, df_words$weights, scale = c(3,.2), #min.freq = 100,
-  #         max.words = input$mt_wc, random.order = FALSE, rot.per = .15, colors = pal2)
-  #}
-  plot_mallet()
-})
+  tmp.1 <- do.call("rbind", tmp.1)
+  tmp.1$topic <- factor(tmp.1$topic, levels = unique(tmp.1$topic))
+
+  mallet_edges <- tmp.1
+
+  mallet_nodes <- unique(c(as.character(mallet_edges$from), as.character(mallet_edges$to)))
+
+  net <- igraph::graph_from_data_frame(mallet_edges, directed=TRUE, vertices= mallet_nodes)
+  ## make a distance
+  V(net)$degree <- degree(net, mode="all")
+  net <- induced_subgraph(net, which(V(net)$degree> input$igraph_semantic_degree))
+  l.1 <- igraph::layout_nicely(net)## change the layout
+  deg <- igraph::degree(net, mode = "all")
+
+  colrs <- unique(c(brewer.pal(12, "Set3"), brewer.pal(12, "Paired"), brewer.pal(8, "Dark2"),brewer.pal(9, "Pastel1")))
+
+  igraph::plot.igraph(net,
+                      layout = l.1,
+                      vertex.color = "#F8F7F2",
+                      vertex.size = deg*igraph_semantic_vertex,
+                      vertex.frame.color = "#F8F7F2",
+                      vertex.label.color = "#538797",
+                      vertex.label.cex = deg*igraph_semantic_label,
+                      edge.arrow.size = 0,
+                      edge.color = colrs[as.numeric(as.factor(E(net)$topic))],
+                      edge.width = as.numeric(as.factor(E(net)$weight))*0.001,
+                      edge.lty = 1
+  )
+  title("mallet semantic network across topic",cex.main=1.5,col.main="#6B8B8D")
+  legend("bottomleft",levels(as.factor(E(net)$topic)), pch=21, col=colrs, pt.bg=colrs, pt.cex=2, cex=.6, bty="n", ncol=1, horiz = F)
+}
+
+
---
+++ R/topic_words.R
@@ -0,0 +1,16 @@
+
+##words in topic
+topic_words <- function(mallet_df, n_topic =7, n_words = 500){
+  topic.words <- mallet.topic.words(mallet_df, smoothed = T, normalized = T)
+  mallet_words_list <- list()
+  for (i in 1:as.numeric(mt_n)) {
+     mallet_words_list[[i]] <- mallet.top.words(mallet_df, topic.words[i,], n_words)
+  }
+  topic_mallet_list <- mallet_words_list
+
+  topic_mallet <- list.cbind(topic_mallet_list)
+  knitr:kable(topic_mallet, "simple")
+}
+
+
+
