def7a253d109553bc4277c10660a1e722490a39a
Local Administrator <LocalAdmin@TUD277510.tudelft.net>
2021-03-31 15:25:34 +0200

    sentiment_multilangauge

--- R/twitter_sentiment.R
+++ R/twitter_sentiment.R
@@ -3,12 +3,27 @@ library(tidytext) #Text mining
 library(tidyr) #Spread, separate, unite, text mining (also included in the tidyverse package)
 library(widyr) #Use for pairwise correlation
 library(textdata)
-
+library(readxl)
 
 twitter_sentiment <- function(df, language, undesirable_words){
-  stopwords <- data.frame(word = stopwords::stopwords(language = language, source = "stopwords-iso"))
+  stopwords <- data.frame(word = stopwords::stopwords(language, source = "stopwords-iso"))
   stopwords$lexicon <- language
   stopwords <- as_tibble(stopwords)
+
+
+  NRC_Emotion_Lexicon <- read_excel("source/NRC-Emotion-Lexicon-v0.92-In105Languages-Nov2017Translations.xlsx")
+  names <- colnames(NRC_Emotion_Lexicon)
+  names <-gsub(".* ","",names)
+  names <-gsub("[()]","",name)
+
+  colnames(NRC_Emotion_Lexicon) <- names
+  NRC_Emotion_Lexicon <- NRC_Emotion_Lexicon %>% select(-"en...1")
+
+  custom_lexicon <- NRC_Emotion_Lexicon %>% select(language, Anger, Anticipation, Disgust,Fear, Joy, Sadness, Surprise, Trust)
+
+ colnames(custom_lexicon) <- c("word", "Anger", "Anticipation", "Disgust","Fear", "Joy", "Sadness", "Surprise", "Trust" )
+
+ custom_lexicon <- custom_lexicon[rowSums(custom_lexicon[,2:9]) >0,]
   #Create tidy text format: Unnested, Unsummarized, -Undesirables, Stop and Short words
   nrc_tidy <- df %>%
   unnest_tokens(word, text) %>% #Break the lyrics into individual words
@@ -17,7 +32,7 @@ twitter_sentiment <- function(df, language, undesirable_words){
   anti_join(stopwords)
 
   youtube_nrc <- nrc_tidy %>%
-  inner_join(get_sentiments("nrc"))
+  inner_join(custom_lexicon)
 }
 
 
