a962431c21b86a3d7a785636515dc7b91c3de754
Local Administrator <LocalAdmin@TUD277510.tudelft.net>
2021-04-08 10:55:06 +0200

    data use

--- data-raw/mydataset.R
+++ data-raw/mydataset.R
@@ -1,5 +1,6 @@
 library(readxl)
 library(dplyr)
+library(usethis)
 NRC_Emotion_Lexicon <- read_excel("source/NRC-Emotion-Lexicon-v0.92-In105Languages-Nov2017Translations.xlsx")
 names <- colnames(NRC_Emotion_Lexicon)
 names <-gsub(".* ","",names)
@@ -7,3 +8,5 @@ names <-gsub("[()]","",names)
 
 colnames(NRC_Emotion_Lexicon) <- names
 NRC_Emotion_Lexicon <- NRC_Emotion_Lexicon %>% select(-"en...1")
+
+usethis::use_data(NRC_Emotion_Lexicon)
