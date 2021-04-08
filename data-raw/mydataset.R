573747eadefb691f3bc7e46690e7819d8d177568
Local Administrator <LocalAdmin@TUD277510.tudelft.net>
2021-04-08 10:34:17 +0200

    add data in package

---
+++ data-raw/mydataset.R
@@ -0,0 +1,9 @@
+library(readxl)
+library(dplyr)
+NRC_Emotion_Lexicon <- read_excel("source/NRC-Emotion-Lexicon-v0.92-In105Languages-Nov2017Translations.xlsx")
+names <- colnames(NRC_Emotion_Lexicon)
+names <-gsub(".* ","",names)
+names <-gsub("[()]","",names)
+
+colnames(NRC_Emotion_Lexicon) <- names
+NRC_Emotion_Lexicon <- NRC_Emotion_Lexicon %>% select(-"en...1")
