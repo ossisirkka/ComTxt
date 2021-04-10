bd03f1dcb846f894cfee84c092de49416d95166b
Local Administrator <LocalAdmin@TUD277510.tudelft.net>
2021-04-10 16:51:39 +0200

    get_tweet_process commit

--- R/get_tweet_preprocess.R
+++ R/get_tweet_preprocess.R
@@ -9,46 +9,91 @@ get_tweet_preprocess <- function(list){
   ##from x$data
   selec_col <- c("created_at","id","text","lang","author_id", "source","conversation_id")
   tmp <- x$data[selec_col]
+  for(i in 1:nrow(x$data)){
+  if("in_reply_to_user_id" %in% colnames(x$data) == TRUE){
+     in_reply_to_user_id <- x$data$in_reply_to_user_id
+  }else{
+    in_reply_to_user_id <- NA}
+  }
+  tmp$in_reply_to_user_id <- in_reply_to_user_id
+
   tmp$geo_coord <- x$data$geo$coordinates$coordinates
   tmp$context_annotations <- x$data$context_annotations
   tmp <- cbind(tmp, x$data$public_metrics)
   tmp.1 <- list()
   for (i in 1:nrow(x$data)){
-    extended_url<- x$data$entities$urls[[i]][[4]]
-    if(length(extended_url)==0){
-      extended_url <- NA
-    } else if (length(extended_url) > 1){
-      extended_url <-paste0(extended_url, sep="", collapse=",")
+    url_t.co <- x$data$entities$urls[[i]][[3]]
+    if(length(url_t.co)==0){
+      url_t.co <- list(NA)
     } else {
-      extended_url <- extended_url
+      url_t.co <- list(url_t.co)
     }
-    hastags <- x$data$entities$hashtags[[i]][[3]]
-    hastags <-paste0(hastags, sep="", collapse=",")
+    tmp.1[[i]] <- url_t.co
+  }
+  tmp$url_t.co <- tmp.1
+  tmp.1 <- list()
+  for (i in 1:nrow(x$data)){
+    url_extended_url<- x$data$entities$urls[[i]][[4]]
+    if(length(url_extended_url)==0){
+      url_extended_url <- list(NA)
+    } else {
+      url_extended_url <- list(url_extended_url)
+    }
+    tmp.1[[i]] <- url_extended_url
+  }
+  tmp$url_extended_url <- tmp.1
+
+  tmp.1 <- list()
+  for (i in 1:nrow(x$data)){
+    hashtags <- x$data$entities$hashtags[[i]][[3]]
+    if(length(hashtags) == 0){
+      hashtags <- list(NA)
+    } else{
+      hashtags <- list(hashtags)
+    }
+    tmp.1[[i]] <- hashtags
+  }
+  tmp$hashtags <- tmp.1
+
+  tmp.1 <- list()
+  for (i in 1:nrow(x$data)){
     mention <- x$data$entities$mentions[[i]][[3]]
     if(length(mention)==0){
-      mention <- NA
-    } else if (length(mention) > 1){
-      mention <-paste0(mention, sep="", collapse=",")
-    } else {
-      mention <- mention
+      mention <- list(NA)
+    }  else {
+      mention <- list(mention)
     }
-    tmp.1[[i]] <- data.frame(extended_url, hastags, mention)
+    tmp.1[[i]] <- mention
   }
-  tmp.1 <- do.call(rbind.data.frame, tmp.1)
-  tmp <- cbind(tmp, tmp.1)
+
+  tmp$mention <- tmp.1
   ## from user data
-  ## colname :[1] "public_metrics","protected" ,"name" ,"entities" , "location", "author_id", "url" ,"description" ,"profile_image_url", "created_at" ,"username" ,"verified","pinned_tweet_id"
+
   names(x$includes$users)[names(x$includes$users) == "id"] <- "author_id"
   df <- left_join(x$data, x$includes$users, by = "author_id")
   ## follower count, following count, tweet count, listed_count
-  tmp$username <-df$username
+  tmp$screen_name <-df$username
   tmp <- cbind(tmp, df$public_metrics.y)
   tmp$profile_image_url <-df$profile_image_url
   tmp$name <-df$name
   tmp$location <-df$location
   tmp$description <-df$description
+  tmp$verified <-df$verified
   tmp$profile_image_url <-df$profile_image_url
-  tmp$created_at.y <-df$created_at.y
+  tmp$account_created_at <-df$created_at.y
+
+  tmp.2 <- list()
+  for (i in 1:length(df$entities.y$url$urls)){
+    profile_url <- df$entities.y$url$urls[[1]][[4]]
+    if(length(profile_url)==0){
+      profile_url <- list(NA)
+    } else {
+      profile_url <- list(profile_url)
+    }
+    tmp.2[[i]] <- data.frame(profile_url)
+  }
+  tmp.2 <- do.call(rbind.data.frame, tmp.2)
+  tmp$profile_url <- tmp.2
   tmp
 })
 
@@ -56,17 +101,28 @@ df_tweets <- do.call(rbind.data.frame, tweets_list_clean)
 colnames(df_tweets)[colnames(df_tweets) == 'id'] <- 'status_id'
 
 ## until Here ---------
-for(i in 1:nrow(df)){
-  if(length(df$geo_coord[[i]]) == 0){
-    df$geo_coord[[i]][[1]] <- NA
-    df$geo_coord[[i]][[2]] <- NA
+geom <- list()
+for(i in 1:nrow(df_tweets)){
+  if(length(df_tweets$geo_coord[[i]]) < 1){
+    df_tweets$geo_coord[[i]] <- c(NA, NA)
   }
-  dd <- df$geo_coord[[i]][1]
-  df$geo_coord[[i]][[1]] <- df$geo_coord[[i]][[2]]
-  df$geo_coord[[i]][[2]] <- dd
+  geom[[i]]<- c(df_tweets$geo_coord[[i]][[2]], df_tweets$geo_coord[[i]][[1]] )
 }
 
+df_tweets <-
+  df_tweets %>%
+  mutate(geo_coord = geom)
 
+col_order <- c("author_id", "status_id", "created_at","screen_name", "text",
+               "source", "conversation_id", "in_reply_to_user_id","mention",
+               "retweet_count","reply_count", "like_count", "quote_count",
+               "hashtags","url_t.co", "url_extended_url","lang","geo_coord",
+               "context_annotations","name","location", "description",
+               "followers_count","following_count", "tweet_count",
+               "listed_count", "account_created_at","verified",
+               "profile_image_url", "profile_url")
+df_tweets <- df_tweets[, col_order]
+df_tweets
 return(df_tweets)
 }
 
