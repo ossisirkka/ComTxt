7d543562d31f5001208670cd629acd7dc162ec3e
Local Administrator <LocalAdmin@TUD277510.tudelft.net>
2021-05-13 15:41:33 +0200

    package load comit user table update

--- R/user_table.R
+++ R/user_table.R
@@ -27,9 +27,20 @@ user_table <- function(df, top_n){
     head(top_n)
 
   tmp_4 <- as.data.frame(sort(table(df$screen_name), decreasing=TRUE))
-  colnames(tmp_4 ) <- "N.tweet_culture"
+  colnames(tmp_4) <- c("screen_name", "N.tweet_culture")
+
+  tmp_4 <- tmp_4 %>%
+      head(top_n)
+
+  for(i in 1:nrow(tmp_4)){
+  tmp_4$verified[[i]] <- unique(tmp[tmp$screen_name == tmp_4$screen_name[[i]],]$verified)
+  }
+
+  tmp_4$verified <- gsub(FALSE, "NO", tmp_4$verified)
+  tmp_4$verified <- gsub(TRUE, "YES", tmp_4$verified)
+  tmp_4 <- tmp_4[c("screen_name","verified", "N.tweet_culture")]
 
   tmp <- cbind(tmp_1, tmp_2, tmp_3, tmp_4)
-  print(kable(tmp, "simple", caption = paste("Top", top_n, "Users in follower, following, tweets number, N tweets about culture")))
+  print(kableExtra::kable(tmp, "simple", caption = paste("Top", top_n, "Users in follower, following, tweets number, N tweets about culture")))
 
 }
